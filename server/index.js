7715d378bec16bde06bda85460c87c4e3d795fc3
Alwin Sunil <64636626+AlwinSunil@users.noreply.github.com>
2025-03-16 01:37:44 +0530

    Updated server code config

--- server/index.js
+++ server/index.js
@@ -15,15 +15,15 @@ app.use(cookieParser());
 app.use(requestLogger);
 
 app.use(
-  cors({
-    origin: process.env.CLIENT_URL,
-    credentials: true,
-  }),
+	cors({
+		origin: process.env.CLIENT_URL,
+		credentials: true,
+	})
 );
 
 app.use("/api/auth", authRoutes);
 app.use("/api/quiz", quizRoutes);
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(process.env.PORT || 3000, () => {
+	console.log("Server is running on port 3000");
 });
